69d3832e9dcfe4bcfd4f57e91fec0185af6811f1
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-02-16 16:24:14 +0900

    Update euclidean_distance.py

--- Geometry/euclidean_distance.py
+++ Geometry/euclidean_distance.py
@@ -5,3 +5,5 @@ def euclidean_distance(pt1, pt2):  # 점1, 점2
     for i in range(len(pt1)):
         result += (pt2[i] - pt1[i]) ** 2
     return result ** 0.5
+
+# print(euclidean_distance([5, 4, 3], [1, 7, 9]))
