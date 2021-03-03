bacff183da6e54eecd02a224a02029d33d2efa10
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-03-03 10:02:35 +0900

    Update swap.py

--- Sorting/swap.py
+++ Sorting/swap.py
@@ -1,5 +1,6 @@
 # 0 인덱스와 1 인덱스의 원소 교체하기
-array = [3, 5]
-array[0], array[1] = array[1], array[0]
+def swap(array):
+  array[0], array[1] = array[1], array[0]
+  return array
 
-print(array)
+print(swap([3, 5]))
