4774a13090f4d16fa69972c866cf542c15d06d06
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-03-03 11:30:17 +0900

    Update quick_sort.py

--- Sorting/quick_sort.py
+++ Sorting/quick_sort.py
@@ -1,5 +1,5 @@
-# 리턴이 없는 버전
-def quick_sort(li, start, end):
+''' 퀵 정렬(Quick Sort) 오름차순 정렬 '''
+def quick_sort(li, start, end):  # 리턴이 없는 버전
     if start >= end:        # 원소가 1개인 경우 종료
         return
     pivot = start           # 피벗은 첫 번째 원소
@@ -12,13 +12,11 @@ def quick_sort(li, start, end):
         # 피벗보다 작은 데이터를 찾을 때까지 반복
         while right > start and li[right] >= li[pivot]:
             right -= 1
-        # 엇갈렸다면 피벗과 교체
-        if left > right:
+        if left > right:  # 엇갈렸다면 작은 데이터와 피벗을 교체
             li[right], li[pivot] = li[pivot], li[right]
-        # 정상적인 상황이라면 left, right 교체
-        else:
+        else:  # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
             li[left], li[right] = li[right], li[left]
-    # 분할 이후 왼쪽 리스트, 오른쪽 리스트 각각 재귀
+    # 분할 이후 왼쪽 리스트, 오른쪽 부분에서 각각 정렬 수행(재귀)
     quick_sort(li, start, right - 1)
     quick_sort(li, right + 1, end)
 
