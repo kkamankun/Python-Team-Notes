858352a0ddc15c3022c5a89c195efaed404d84c8
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-03-03 09:34:50 +0900

    Update selection_sort.py

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -1,13 +1,12 @@
-# Selection Sort 오름차순 정렬
-
+''' 선택 정렬(Selection Sort) 오름차순 정렬 '''
 def selection_sort(li):   # 파라미터로 1차원 리스트
     li_len = len(li)      # 리스트의 길이
     for i in range(li_len):
-        min_idx = i       # 가장 왼쪽 인덱스 저장
+        min_idx = i       # 가장 왼쪽 인덱스의 인덱스
         for j in range(i+1, li_len):  # 가장 작은 원소의 인덱스 탐색
             if li[min_idx] > li[j]:   # 탐색하며 업데이트
                 min_idx = j
-        # 가장 작은 원소와 스왑
+        # 가장 작은 원소와 스와프
         li[i], li[min_idx] = li[min_idx], li[i]
     return li
 
