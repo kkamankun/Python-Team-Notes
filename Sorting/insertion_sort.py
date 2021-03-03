4b1724c0080e63354042977fc6c3dc117d504ac9
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-03-03 09:51:18 +0900

    Update insertion_sort.py

--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -1,12 +1,11 @@
-# Insertion Sort 오름차순 정렬
-
+''' 삽입 정렬(Insertion Sort) 오름차순 정렬 '''
 def insertion_sort(li):
     li_len = len(li)   # 1차원 리스트 길이
     for i in range(1, li_len):
-        for j in range(i, 0, -1):  # 현재위치부터 왼쪽으로 이동하며 탐색
-            if li[j] < li[j-1]:    # 한칸씩 왼쪽으로 이동
+        for j in range(i, 0, -1):  # 현재 위치부터 왼쪽으로 이동하며 탐색
+            if li[j] < li[j-1]:    # 한 칸씩 왼쪽으로 이동
                 li[j], li[j - 1] = li[j - 1], li[j]  # 이동을 swap 으로 구현
-            else:    # 자신보다 작은 데이터를 만나면 그 위치에서 멈춤
+            else:    # 자기보다 작은 데이터를 만나면 그 위치에서 멈춤
                 break
 
 
