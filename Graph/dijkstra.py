0165d174c8ea0eac443d80407490c69ea3704a16
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-03-10 13:34:57 +0900

    시간 복잡도 O(V^2)

--- Graph/dijkstra.py
+++ Graph/dijkstra.py
@@ -1,28 +1,31 @@
+''' 간편한 다익스트라 알고리즘 소스코드 '''
 import sys
-
-INF = int(1e9)
+INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
 
 # 노드의 개수, 간선의 개수를 입력받기
-node, edge = map(int, sys.stdin.readline().rstrip().split())
+node_num, edge_num = map(int, sys.stdin.readline().rstrip().split())
 
-# 시작 노드 입력받기
+# 시작 노드 번호를 입력받기
 start = int(sys.stdin.readline().rstrip())
 
-# 그래프 생성, 방문 노드 체크 리스트 생성, 최단거리 테이블 생성 및 무한으로 초기화
-graph = [[] for _ in range(node + 1)]
-visited = [False] * (node + 1)
-distance = [INF] * (node + 1)
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for _ in range(node_num + 1)]
+# 방문한 적이 있는지 체크하는 목적의 리스트를 만들기
+visited = [False] * (node_num + 1)
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (node_num + 1)
 
 # 모든 간선 정보 입력받기
-for _ in range(edge):
+for _ in range(edge_num):
     src, des, weight = map(int, sys.stdin.readline().rstrip().split())
     graph[src].append((des, weight))
 
 
+# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
 def get_smallest_node():
     min_value = INF
     index = 0
-    for i in range(1, node + 1):
+    for i in range(1, node_num + 1):
         if distance[i] < min_value and not visited[i]:
             min_value = distance[i]
             index = i
@@ -30,29 +33,31 @@ def get_smallest_node():
 
 
 def dijkstra(start):
-    # 시작 노드 초기화
+    # 시작 노드에 대해서 초기화
     distance[start] = 0
     visited[start] = True
     for j in graph[start]:
         distance[j[0]] = j[1]
-    # 시작 노드를 제외한 전체 n-1 개의 노드에 대해 반복
-    for i in range(node-1):
+    # 시작 노드를 제외한 전체 n - 1 개의 노드에 대해 반복
+    for i in range(node_num - 1):
         # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
         now = get_smallest_node()
         visited[now] = True
         # 현재 노드와 연결된 다른 노드를 확인
-        for adj_node in graph[now]:
-            cost = distance[now] + adj_node[1]
-            # 기존 값보다 더 짧은 경우
-            if cost < distance[adj_node[0]]:
-                distance[adj_node[0]] = cost
+        for adj in graph[now]:
+            cost = distance[now] + adj[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[adj[0]]:
+                distance[adj[0]] = cost
 
-
+# 다익스트라 알고리즘 수행
 dijkstra(start)
 
-# 결과 출력
-for i in range(1, node + 1):
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, node_num + 1):
+    # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
     if distance[i] == INF:
         print("무한")
+    # 도달할 수 있는 경우 거리를 출력
     else:
         print(distance[i])
